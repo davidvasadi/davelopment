c50cf9574147beca997921f83fe37683d66f63ce
Dave <david.vasadi@icloud.com>
2025-10-20 20:14:44 +0200

    first deploy

--- backend/src/api/pricing-section/routes/pricing-section.ts
+++
@@ -1,7 +0,0 @@
-/**
- * pricing-section router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::pricing-section.pricing-section');
--- backend/src/api/pricing-section/services/pricing-section.ts
+++
@@ -1,7 +0,0 @@
-/**
- * pricing-section service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::pricing-section.pricing-section');
