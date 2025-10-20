a3078511ecdbc7de2b1f4b2ceefd1e9e9d46c85a
Dave <david.vasadi@icloud.com>
2025-10-20 21:13:34 +0200

    feat(backend): Strapi app + prod config (/dashboard, url, keys)

---
+++ backend/config/server.ts
@@ -0,0 +1,13 @@
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+export default ({ env }) => ({
+  host: env('HOST', '127.0.0.1'),
+  port: env.int('PORT', 1337),
+  url: env('URL', 'https://davelopment.hu'),
+  app: { keys: env.array('APP_KEYS') },
+});
